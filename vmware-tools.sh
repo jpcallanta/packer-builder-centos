689cce5429ef09a62f4300f81c8e990b30193d17
John <jp@callanta.net>
2014-04-05 20:00:30 -0400

    Working vagrant user shell script and modified kickstart script with less packages to install

--- vmware-tools.sh
+++ vmware-tools.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+# Install Open VMware Tools repo
+echo "Installing open-vm-tools..."
+rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub
+rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub
+echo -e "[vmware-tools]\nname=VMware Tools\nbaseurl=http://packages.vmware.com/tools/esx/4.1latest/rhel6/\$basearch\nenabled=1\ngpgcheck=1" > /etc/yum.repos.d/vmware-tools.repo
+yum -y update
+yum -y install vmware-open-vm-tools-nox
